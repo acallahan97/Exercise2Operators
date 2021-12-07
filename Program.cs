f96713486f6068badf78ef42f472e5e83f5bf4b5
acallahan97 <callahanamanda877@gmail.com>
2021-12-07 08:37:11 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise2Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is the area of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            AreaOfCircle(radius);
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+       
+    }
+}
